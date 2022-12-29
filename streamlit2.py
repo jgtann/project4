9b7766b73da162ffa572b75acd67153c78ad013f
jgtann <56464589+jgtann@users.noreply.github.com>
2022-12-29 17:26:17 +0800

    Update streamlit2.py

--- streamlit2.py
+++ streamlit2.py
@@ -3,8 +3,6 @@ import pandas as pd
 import numpy as np 
 import pickle # to load a saved model
 import base64 # to open .gif files in streamlit
-import sklearn
-
 
 # Set the navigation
 # Home page: show data selected, preparation and modelling
